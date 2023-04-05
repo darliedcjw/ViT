b8627e83f952ec60ea670ef7c18482f8a100c0c3
darliedcjw <87639116+darliedcjw@users.noreply.github.com>
2023-04-05 13:53:34 +0800

    Update

--- scripts.py
+++ scripts.py
@@ -11,6 +11,8 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor, Compose
 from torchvision.datasets.mnist import MNIST
 
+from vit import ViT
+
 np.random.seed(0)
 torch.manual_seed(0)
 
@@ -27,4 +29,59 @@ def main():
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using: {}'.format(device))
+
+    model = ViT(image_res=(1, 28, 28), n_patches=7, n_blocks=2, hidden_d=8, n_heads=2, out_d=10).to(device)
+    epochs = 5
+    lr = 0.005
+
+    optimizer = Adam(model.parameters(), lr=lr)
+    criterion = CrossEntropyLoss()
+
+    for epoch in trange(epochs, desc='Training'):
+        
+        train_loss = 0.0
+        train_acc = 0.0
+
+        # Training
+        model.train()
+
+        for batch in tqdm(train_loader, desc='Epoch {} in training'.format(epoch+1)):
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            y_pred = model(x)
+            loss = criterion(y_pred, y)
+
+            train_loss += loss.detach().cpu().item()
+
+            train_acc += torch.sum(torch.argmax(y_pred, dim=1) == y).detach().cpu().item() / len(y)
+
+            optimizer.zero_grad()
+            
+            loss.backward()
+
+            optimizer.step()
+
+        print('Epoch {}: \nTrain Loss - {} \nAccuracy - {}'.format(epoch + 1, train_loss / len(train_loader), train_acc / len(train_loader)))
+
+        # Eval
+        model.eval()
+
+        with torch.no_grad():
+            val_loss = 0.0
+            val_accuracy = 0.0
+            for batch in tqdm(val_loader, desc='Validating'):
+                x, y = batch
+                x, y = x.to(device), y.to(device)
+                y_pred = model(x)
+                loss = criterion (y_pred, y)
+
+                val_loss += loss.detach().cpu().item()
+
+                val_accuracy += torch.sum(torch.argmax(y_pred, dim=1) == y).detach().cpu().item() / len(y)
+            
+        print('Epoch {}: \nTrain Loss - {} \nAccuracy - {}'.format(epoch + 1, val_loss / len(val_loader), val_accuracy / len(val_loader)))
+
+if __name__ == '__main__':
+    main()
+
     
--- vit.py
+++ vit.py
@@ -2,13 +2,91 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+'''
+Batchnorm2d calculates mean and sd with respect to each channel for the batch
+LayerNorm calculates mean and sd with respect to last D dimensions of normalized shape
+'''
+
+'''
+hstack: concatenate column wise
+vstack: concatenate row wise
+stack: concatenate batch
+'''
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class MSA(nn.Module):
+    def __init__(self, hidden_d, n_heads=2):
+        super(MSA, self).__init__()
+        self.hidden_d = hidden_d
+        self.n_heads = n_heads
+
+        # Number of heads for different standards of computation
+        assert hidden_d % n_heads == 0 
+
+        self.d_head = int(hidden_d / n_heads)
+        # nn.Linear is a module type and is therefore stored as a module list
+        self.q_mappings = nn.ModuleList([nn.Linear(self.d_head, self.d_head) for _ in range(self.n_heads)])
+        self.k_mappings = nn.ModuleList([nn.Linear(self.d_head, self.d_head) for _ in range(self.n_heads)])
+        self.v_mappings = nn.ModuleList([nn.Linear(self.d_head, self.d_head) for _ in range(self.n_heads)])
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, images):
+        '''
+        Sequences: N x Seq Len x Token Dim
+        Multi-Head: N x Seq Len x Token Dim / n_heads
+
+        Patchs: B x Patches x Flatten Pixel
+        **Flatten Pixel is treated as token Token Dim
+        '''
+        result = []
+
+        for image in images:
+            image_result = []
+            for head in range(self.n_heads):
+                q_mapping = self.q_mappings[head]
+                k_mapping = self.k_mappings[head]
+                v_mapping = self.v_mappings[head]
+
+                # Working on splitting the head on each patch
+                patches = image[:, head * self.d_head: (head + 1) * self.d_head]
+                q, k, v = q_mapping(patches), k_mapping(patches), v_mapping(patches)
+
+                # Relevance: Dimension is 50 x 50 
+                attention = self.softmax(q @ k.T / (self.d_head ** 0.5))
+                # Giving the sum of weighted value: Dimension is 50 x 4
+                image_result.append(attention @ v)
+            result.append(torch.hstack(image_result))
+        return torch.stack(result)
+    
+class EncoderBlock(nn.Module):
+    def __init__(self, hidden_d, n_heads, mlp_ratio=4):
+        super(EncoderBlock, self).__init__()
+        self.hidden_d = hidden_d
+        self.n_head = n_heads
+        self.ln1 = nn.LayerNorm(hidden_d)
+        self.mhsa = MSA(hidden_d=self.hidden_d, n_heads=self.n_head)
+        self.ln2 = nn.LayerNorm(hidden_d)
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_d, mlp_ratio * hidden_d),
+            nn.GELU(),
+            nn.Linear(mlp_ratio * hidden_d, hidden_d)
+        )
+
+    def forward(self, x):
+        out = x + self.mhsa(self.ln1(x))
+        out = out + self.mlp(self.ln2(out))
+        return out
+
 class ViT(nn.Module):
-    def __init__(self, image_res=(1, 28, 28), n_patches=7, hidden_d=8):
+    def __init__(self, image_res=(1, 28, 28), n_patches=7, hidden_d=8, n_blocks=2, n_heads=2, out_d=10):
         super(ViT, self).__init__()
         self.channel = image_res[0]
         self.height = image_res[1]
         self.width = image_res[2]
         self.n_patches = n_patches
+        self.n_blocks = n_blocks
+        self.n_heads = n_heads
         self.hidden_d = hidden_d
 
         assert self.height % n_patches == 0, 'Height not cleanly divided!'
@@ -16,23 +94,25 @@ class ViT(nn.Module):
  
         self.patch_size = self.height // self.n_patches, self.width // self.n_patches
 
-        # Linear Mapping (Flatten)
+        # 1. Linear Mapping (Flatten)
         self.linear_input = self.patch_size[0] * self.patch_size[1] * self.channel
         self.linear = nn.Linear(self.linear_input, self.hidden_d)
 
-        # Layer Normalization (Normalise based on the last dimension)
-        '''
-        Batchnorm2d calculates mean and sd with respect to each channel for the batch
-        LayerNorm calculates mean and sd with respect to last D dimensions of normalized shape
-        '''
-        self.ln = nn.LayerNorm(hidden_d)
-
-        # Learnable Classification Token
+        # 2. Learnable Classification Token
         self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
 
-        # Positional Embedding
+        # 3. Positional Embedding
         self.pos_embed = nn.Parameter(torch.tensor(self.get_positional_embeddings(self.n_patches ** 2 + 1, self.hidden_d)))
-        self.pos_embed.requires_grad = False        
+        self.pos_embed.requires_grad = False
+
+        # 4. Transformer Encoder Blocks
+        self.encoder_blocks = nn.ModuleList([EncoderBlock(hidden_d=self.hidden_d, n_heads=self.n_heads) for _ in range(self.n_blocks)])
+
+        # 5. Classification MLP
+        self.mlp = nn.Sequential(
+            nn.Linear(self.hidden_d, out_d),
+            nn.Softmax(dim=-1)
+        )
 
     def patchify(self, images, n_patches):
         num_images, _, _, _ = images.shape
@@ -40,7 +120,7 @@ class ViT(nn.Module):
         assert self.height == self.width, 'Implemented only for square images for now!'
         
         # Gives Batch x Number of Patches x Number of Pixels in a Patch
-        patches = torch.zeros(num_images, n_patches ** 2, self.height * self.width * self.channel//n_patches ** 2) 
+        patches = torch.zeros(num_images, n_patches ** 2, self.height * self.width * self.channel//n_patches ** 2).to(device)
 
         # Iterating through each image in a batch
         for idx, image in enumerate(images):
@@ -66,14 +146,20 @@ class ViT(nn.Module):
         patches = self.patchify(images, self.n_patches)
         tokens = self.linear(patches)
         tokens = torch.stack([torch.vstack((self.class_token, tokens[i])) for i in range(len(tokens))])
-        pos_emb = self.pos_embed.repeat(images.shape[0], 1, 1)
-        out = tokens + pos_emb
-        out = self.ln(out)
+        out = tokens + self.pos_embed.repeat(images.shape[0], 1, 1)
+
+        for block in self.encoder_blocks:
+            out = block(out)
+        
+        out = self.mlp(out)
+
+        out = out[:, 0]
+        
         return out
-        # return tokens
 
 if __name__ == '__main__':
-    model = ViT(image_res=(3, 28, 28), n_patches=7)
+    model = ViT(image_res=(3, 28, 28), n_patches=7, hidden_d=8, n_blocks=2, n_heads=2, out_d=10)
     x = torch.randn(24, 3, 28, 28)
+    
     output = model(x)
     print(output.shape)
